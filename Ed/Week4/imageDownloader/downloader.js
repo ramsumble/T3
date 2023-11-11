9d3239934e776d9e21ab45916a0e2004163c0b51
ramsumble <ram.sumble@gmail.com>
2023-11-11 11:04:10 +1100

    get picture url function

--- Ed/Week4/imageDownloader/downloader.js
+++ Ed/Week4/imageDownloader/downloader.js
@@ -12,20 +12,33 @@ const { finished } = require("node:stream/promises");
 
 const path = require("node/path");
 
+const API_URL_BASE = "https://pokeapi.co/api/v2/pokemon/"
+
 function downloadPokemonPicture (targetId = getRandomPokemonId()) {
 
 }
 
 function getRandomPokemonId() {
-
+    return Math.floor(Math.random() + 1017) + 1;
 }
 
 // get pokemon data and image url 
-function getPokemonPictureUrl(targetId = getRandomPokemonId()) {
+async function getPokemonPictureUrl(targetId = getRandomPokemonId()) {
+    // get API data
+    let response = await fetch(API_URL_BASE + targetId);
+
+    // convert to usable json
+    let data = await response.json();
+
+    // can just return the value rather than storing to variable and returning variable
+    // let imageURL = data.sprites.other["official-artwork"].front_default;
+    // return imageURL
+
+    return data.sprites.other["official-artwork"].front_default;
 
 }
 
-function savePokemonPictureToDisk(targetUrl, targetDownloadFilename, targetDownloadDirectory = "./") {
+async function savePokemonPictureToDisk(targetUrl, targetDownloadFilename, targetDownloadDirectory = "./") {
 
 }
 
