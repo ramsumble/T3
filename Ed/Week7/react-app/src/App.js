ff948b5101383ef7a069a82366a0d924a74efa0c
ramsumble <ram.sumble@gmail.com>
2023-11-30 20:02:13 +1100

    comments + local storage to state

--- Ed/Week7/react-app/src/App.js
+++ Ed/Week7/react-app/src/App.js
@@ -1,21 +1,35 @@
 import { useLocalStorage } from 'react-use';
 import './App.css';
-import { useState } from 'react';
-import { useEffect } from 'react';
-
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [storeMessage, setStoredMessage] = useLocalStorage('message', "")
-  const [message, setMessage] = useState("")
+  const [storedMessage, setStoredMessage] = useLocalStorage('message', "");
+  const [message, setMessage] = useState("");
+
+  // Component is loaded
+  // gap of time
+  // Component is rendered
+  // gap of time
+  // ComponentDidMount -> useEffect(()=>{}, [])
+  // gap of time
+  // ComponentWillUpdate -> useEffect(()=> {}, [message])
+
+
 
   useEffect(() => {
-    setMessage(storeMessage);
-    // eslint-disable-next-line 
+    setMessage(storedMessage);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  //Everytime state message changes, we will update storageMessage value
+  useEffect(() => {
+    setStoredMessage(message);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [message])
 
   return (
     <div className="App">
-      <h1>{message}</h1>
+      {message && <h1>{message}</h1>}
     </div>
   );
 }
